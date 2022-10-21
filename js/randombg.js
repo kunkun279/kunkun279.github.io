73471045924589e0ed65141545fe6f043e177827
kunkun279 <2793826691@qq.com>
2022-10-21 14:50:32 +0800

    Site updated: 2022-10-21 14:50:22

---
+++ js/randombg.js
@@ -0,0 +1,20 @@
+//随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+var backimg =[
+  "url(https://w.wallhaven.cc/full/rr/wallhaven-rr3zvm.png)"
+];
+//获取背景图片总数，生成随机数
+var bgindex =Math.floor(Math.random() * backimg.length);
+//重设背景图片
+document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+//随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+var bannerimg =[
+  "url(/medias/banner/1.jpg)",
+  "url(/medias/banner/2.jpg)",
+  "url(/medias/banner/3.jpg)",
+  "url(/medias/banner/6.jpg)",
+  "url(/medias/banner/4.jpg)"
+];
+//获取banner图片总数，生成随机数
+var bannerindex =Math.floor(Math.random() * bannerimg.length);
+//重设banner图片
+document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
